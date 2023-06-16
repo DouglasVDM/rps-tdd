81c303a368f284df904c50f26fb937570528bcbf
Douglas Van Der Merwe <douglasvdmerwe@gmail.com>
2023-06-16 16:51:47 +0200

    Sevent test - rock vs. lizard

--- index.test.js
+++ index.test.js
@@ -1,74 +1,98 @@
 const rps = (left, right) => {
-    if (left === right){
-        return "draw";
-    }
-    return (
-        (left === "rock" && right === "scissors")
-        || (left === "scissors" && right === "paper")
-        || (left === "paper" && right === "rock")
-    )
-        ? "left"
-        : "right";
+  const outcomes = {
+    rock: { beats: ["scissors", "lizard"] },
+    paper: { beats: ["rock", "spock"] },
+    scissors: { beats: ["paper", "lizard"] },
+    lizard: { beats: ["paper", "spock"] },
+    spock: { beats: ["rock", "scissors"] },
+  };
+
+  if (left === right) {
+    return "draw";
+  }
+
+  if (outcomes[left].beats.includes(right)) {
+    return "left";
+  } else {
+    return "right";
+  }
 };
 
-describe("rock, paper, scissors", () => {
-    it("should say left wins for rock vs. scissors", () => {
-        const left = "rock";
-        const right = "scissors";
+describe("Rock, Paper, Scissors, Lizard, Spock", () => {
+  
+    // TEST 1
+  it("should say left wins for rock vs. scissors", () => {
+    const left = "rock";
+    const right = "scissors";
 
-        const outcome = rps(left, right);
+    const outcome = rps(left, right);
 
-        expect(outcome).toBe("left");
+    expect(outcome).toBe("left");
+  });
+
+  // TEST 2
+  it("should say right wins for scissors vs. rock", () => {
+    const left = "scissors";
+    const right = "rock";
+
+    const result = rps(left, right);
+
+    expect(result).toBe("right");
+  });
+
+  // TEST 3
+  it("should say left wins for scissots vs. paper", () => {
+    const left = "scissors";
+    const right = "paper";
+
+    const result = rps(left, right);
+
+    expect(result).toBe("left");
+  });
+
+  // TEST 4
+  it("should say right wins for paper vs. scissors", () => {
+    const left = "paper";
+    const right = "scissors";
+
+    const result = rps(left, right);
+
+    expect(result).toBe("right");
+  });
+
+  // TEST 5
+  it("should say left wins for paper vs. rock", () => {
+    const left = "paper";
+    const right = "rock";
+
+    const result = rps(left, right);
+
+    expect(result).toBe("left");
+  });
+
+  // TEST 6
+  it("should say right wins for rock vs, paper", () => {
+    const left = "rock";
+    const right = "paper";
+
+    const result = rps(left, right);
+
+    expect(result).toBe("right");
+  });
+
+  // TEST 7
+  it("should say left wins for rock vs. lizard", () => {
+    const left = "rock";
+    const right = "lizard";
+
+    const outcome = rps(left, right);
+
+    expect(outcome).toBe("left");
+  });
+
+   ["rock", "paper", "scissors", "lizard", "spock"].forEach((both) => {
+    it(`should say draw for ${both} vs. ${both}`, () => {
+      expect(rps(both, both)).toBe("draw");
     });
-
-    it("should say right wins for scissors vs. rock", () => {
-        const left = "scissors";
-        const right = "rock";
-
-        const outcome = rps(left, right);
-
-        expect(outcome).toBe("right");
-    });
-
-    it("should say left wins for scissors vs. paper", () => {
-        const left = "scissors";
-        const right = "paper";
-
-        const outcome = rps(left, right);
-
-        expect(outcome).toBe("left");
-    });
-
-    it("should say right wins for paper vs. scissors", () => {
-        const left = "paper";
-        const right = "scissors";
-
-        const outcome = rps(left, right);
-
-        expect(outcome).toBe("right");
-    });
-
-    it("should say left wins paper vs. rock", () => {
-        const left = "paper";
-        const right = "rock";
-
-        const result = rps(left, right);
-
-        expect(result).toBe("left");
-    });
-
-    it("should say right wins rock vs. paper", () => {
-        const left = "rock";
-        const right = "paper";
-
-        const result = rps(left, right);
-
-        expect(result).toBe("right");
-    });
-
-    ["rock", "paper", "scissors"].forEach((both) => {
-        it(`should sat draw for ${both} vs. ${both}`, () => {
-            expect(rps(both, both)).toBe("draw");
-        });
-    });
+  });
 });
